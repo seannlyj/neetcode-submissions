368c5f51bfb876453eda7769803754448903f5f9
sean <42329771+seannlyj@users.noreply.github.com>
2026-05-27 16:09:14 +0800

    Add: max-area-of-island - submission-17

---
+++ Data Structures & Algorithms/max-area-of-island/submission-17.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool isValid(vector<vector<int>>& grid, int x, int y){
+        return x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size();
+    }
+
+    int bfs(vector<vector<int>>& grid, int startingX, int startingY){
+        int islandMaxArea = 0;
+
+        queue<array<int, 2>> q;
+        q.push({startingX, startingY});
+        grid[startingX][startingY] = 0;
+
+        while(!q.empty()){
+            array<int, 2> pos = q.front();
+            q.pop();
+
+            islandMaxArea++;
+
+            vector<array<int, 2>> neighbours = 
+            {{pos[0] + 1, pos[1]}, 
+            {pos[0] - 1, pos[1]}, 
+            {pos[0], pos[1] + 1}, 
+            {pos[0], pos[1] - 1}};
+
+
+            for(auto& neighbour : neighbours){
+                if(isValid(grid, neighbour[0], neighbour[1])){
+                    if(grid[neighbour[0]][neighbour[1]] == 1) {
+                        q.push({neighbour[0], neighbour[1]});
+                        grid[neighbour[0]][neighbour[1]] = 0;
+                    }
+                } 
+            }
+        }
+
+        return islandMaxArea;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxArea = 0;
+        //vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[i].size(); j++){
+                if(grid[i][j] == 1){
+                    int islandMaxArea = bfs(grid, i, j);
+                    maxArea = max(maxArea, islandMaxArea);
+
+                }
+            }
+        }
+
+        return maxArea;
+    }
+};
