53a430763568d1f8dc525a16b04b047936308d8d
sean <42329771+seannlyj@users.noreply.github.com>
2026-06-01 16:50:14 +0800

    Add: buy-and-sell-crypto - submission-12

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-12.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int r = 0;
+
+        int profit = 0;
+
+        while(r < prices.size()){
+            if(prices[l] > prices[r]){
+                l = r;
+            } else if (prices[l] < prices[r]) {
+                profit = max(profit, prices[r] - prices[l]);
+            }
+            r++;
+        }
+
+        return profit;
+    }
+};
