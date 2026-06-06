509b27e809b983eac5dcbbe16d0676ee5bd34756
sean <42329771+seannlyj@users.noreply.github.com>
2026-06-06 15:23:11 +0800

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+// What is a well formed parenthesis?
+// (): opening ( will always have a closing )
+
+//choice: put ( or )
+//prune when: there isn't a well formed parenthesis 
+//completed when: no. of parenthesises made == 2n
+public:
+    void backtracking(int n, int open, int close, string& curr, vector<string>& result){
+        //prune if more close than open
+        if(close > open) return;
+
+        // complete
+        if(open+close == 2*n){
+            result.push_back(curr);
+        }
+
+        //choice: choosing ( or ) to put in string
+        if(open < n){
+            curr.push_back('(');
+            backtracking(n, open + 1, close, curr, result);
+            curr.pop_back();
+        }
+
+        if(close < open){
+            curr.push_back(')');
+            backtracking(n, open, close + 1, curr, result);
+            curr.pop_back();
+        }
+
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string curr;
+        backtracking(n, 0, 0, curr, result);
+        return result;
+    }
+};
