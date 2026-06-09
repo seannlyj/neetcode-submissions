3b7a01b19653f4edf8126efe71b9eefe606df752
sean <42329771+seannlyj@users.noreply.github.com>
2026-06-09 16:12:20 +0800

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        for(int i = 0; i < gas.size(); i++){
+            int startingStation = i;
+            int currentStation = i;
+            int currGas = gas[i];
+
+            while(currGas > 0){
+                //reduce currgas to go to next station
+                currGas -= cost[currentStation];
+
+                if(currGas < 0) break;
+
+                currentStation++;
+                //wrap around
+                if(currentStation == gas.size()){ 
+                    currentStation = 0;
+                }
+                
+                // check if we did a loop
+                if(currentStation == startingStation) {
+                    return startingStation;
+                }
+
+                //add gas to refill from station
+                currGas += gas[currentStation];
+            }
+        }
+
+        return -1;
+    }
+};
