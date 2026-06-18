7a9dd9895f89531c66e8a241de27ef5120fe3c1e
sean <42329771+seannlyj@users.noreply.github.com>
2026-06-18 17:45:02 +0800

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> result(temperatures.size(), 0);
+        stack<pair<int, int>> mono; // <temperature[i], i>
+       
+        for(int i = 0; i < temperatures.size(); i++) {
+            if(mono.empty()) mono.push({temperatures[i], i});
+            else {
+                while(!mono.empty() &&  mono.top().first < temperatures[i]){
+                    int distance = i - mono.top().second;
+                    result[mono.top().second] = distance;
+                    mono.pop();
+                }
+                mono.push({temperatures[i], i});
+            }
+        }
+
+        return result;
+    }
+};
