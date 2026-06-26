a99681690e6a2ee19b615f09e8cd102d5f3dc670
sean <42329771+seannlyj@users.noreply.github.com>
2026-06-26 10:32:59 +0800

    Add: minimum-size-subarray-sum - submission-2

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+//dynamic sliding window
+// shrink when: removing front element still satisfies >= target
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int l = 0;
+        int sum = 0;
+        int length = INT_MAX;
+
+        for(int r = 0; r < nums.size(); r++){
+            sum += nums[r];
+            
+            while(sum > target){
+                if(sum - nums[l] < target) break;
+
+                sum -= nums[l];
+                l++;
+            }
+
+            if(sum >= target)
+                length = min(length, r-l + 1);
+        }
+
+        if(sum < target) return 0;
+
+        return length;
+    }
+};
