ff5f6744275e2946d4970a893a4a64d739f84cea
sean <42329771+seannlyj@users.noreply.github.com>
2026-06-26 13:36:05 +0800

    Add: find-k-closest-elements - submission-4

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-4.cpp
@@ -0,0 +1,32 @@
+class Solution {
+// static sliding window
+// build first window (calculate difference between x and arr[i] and sum all up)
+// iterate through
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int bestSum = 0;
+        int sum = 0;
+        vector<int> result;
+
+        int l = 0;
+        // build first window
+        for(int i = 0; i < k; i++){
+            sum += abs(x - arr[i]);
+            result.push_back(arr[i]);
+        }
+        bestSum = sum;
+
+        for(int i = k; i < arr.size(); i++){
+            sum -= abs(x - arr[l]); // remove front element
+            sum += abs(x - arr[i]); // add new element
+            if(sum < bestSum){
+                bestSum = sum;
+                result.erase(result.begin());
+                result.push_back(arr[i]);
+            }
+            l++;
+        }
+
+        return result;
+    }
+};
