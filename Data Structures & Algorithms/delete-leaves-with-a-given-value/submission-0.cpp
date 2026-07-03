af9de39daad8bbc226ab424995abb2bc61bff91b
sean <42329771+seannlyj@users.noreply.github.com>
2026-07-03 14:52:51 +0800

    Add: delete-leaves-with-a-given-value - submission-0

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-0.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+// probably post order dfs
+
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        // Base case
+        if(root == nullptr) {
+            return nullptr;
+        }
+
+        if(root->left) root->left = removeLeafNodes(root->left, target);
+        if(root->right) root->right = removeLeafNodes(root->right, target);
+
+        if(root->val == target && root->left == nullptr && root->right == nullptr){
+            cout<<"This is a leaf node that matches the target " << target <<", deleting it"<<endl;
+            return nullptr;
+        }
+
+        return root;
+    }
+};
