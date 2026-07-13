b344af2872bcd2f869806999357d9a2eaebde484
sean <42329771+seannlyj@users.noreply.github.com>
2026-07-13 15:07:47 +0800

    Add: asteroid-collision - submission-6

---
+++ Data Structures & Algorithms/asteroid-collision/submission-6.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s;
+
+        for(int& a : asteroids){
+            bool p = true;
+            while(!s.empty() && a < 0 && s.top() > 0){
+                int ast1 = s.top();
+                int ast2 = abs(a);
+                if(ast2 > ast1){
+                    s.pop();
+                } else if (ast2 < ast1){
+                    p = false;
+                    break;
+                } else {
+                    s.pop();
+                    p = false;
+                    break;
+                }
+            }
+
+            if(a != 0 && p) s.push(a);
+        }
+
+        vector<int> result;
+
+        while(!s.empty()){
+            cout<<s.top()<<endl;
+            result.push_back(s.top());
+            s.pop();
+        }
+
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
