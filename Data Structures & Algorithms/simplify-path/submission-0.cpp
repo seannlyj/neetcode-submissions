ae1ece30759a6fb38bc7ce6d2f5d2bc7c2aa3dd2
sean <42329771+seannlyj@users.noreply.github.com>
2026-07-14 17:19:39 +0800

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        // slashes = signifies start of next file's directory
+        // double period = pop
+        // single period = ignore
+        // anything else = push
+
+        vector<string> s;
+
+        int i = 0;
+        while(i < path.size()){
+            //skip brackets
+            while(i < path.size() && path[i] == '/') i++;
+            int tokenStart = i;
+            while(i < path.size() && path[i] != '/') i++;
+            int tokenLength = i - tokenStart;
+            string token = path.substr(tokenStart, tokenLength);
+
+            if(token == "..") {
+                if(!s.empty()) s.pop_back();
+            } else if(token != "." && token.length() > 0) {
+                cout<<"Token = " << token <<", size " << token.length() << endl;
+                s.push_back(token);
+            }
+        }
+
+        // construct string
+        string result = "/";
+
+        for(int j = 0; j < s.size(); j++){
+            result+=s[j];
+
+            if(j + 1 < s.size()){
+                result += "/";
+            }
+
+            cout<<j<<endl;
+            cout<<s[j]<<endl;
+
+        }
+        cout<<result<<endl;
+
+        return result;
+    }
+};
