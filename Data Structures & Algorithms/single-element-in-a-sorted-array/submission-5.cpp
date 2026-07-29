a3b5da51990a7f55b58a4e16be0e993de3d673ae
sean <42329771+seannlyj@users.noreply.github.com>
2026-07-29 13:51:28 +0800

    Add: single-element-in-a-sorted-array - submission-5

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-5.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while(l < r){
+            int m = l + (r-l)/2;
+
+            // m is even, it's pair should be at m + 1
+            if(m % 2 == 0){
+                // if it is indeed the pair, then the target is on the right
+                if(nums[m + 1] == nums[m]) {
+                    l = m + 2;
+                } 
+                // if it is not the pair, then something on the left has messed up the order
+                else {
+                    r = m;
+                }
+            } 
+            // m is odd, it's pair should be at m - 1;
+            else {
+                //if same, target should be on right
+                if(nums[m - 1] == nums[m]){
+                    l = m + 1;
+                } else {
+                    r = m;
+                }
+            }
+        }
+
+        return nums[l];
+    }
+};
